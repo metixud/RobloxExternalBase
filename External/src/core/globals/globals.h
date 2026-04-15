4e4b26bf52d6d64fd3247d7e0a3e5956276c2858
metixud <metixsud@gmail.com>
2026-04-15 19:06:33 +0200

    Update globals.h

--- External/src/core/globals/globals.h
+++ External/src/core/globals/globals.h
@@ -6,7 +6,7 @@ namespace Globals {
     inline RBX::RenderEngine renderEngine(0);
     inline RBX::RbxInstance workspace(0);
     inline RBX::RbxInstance players(0);
-
+    inline bool running = true;
     inline RBX::RbxInstance camera(0);
     inline RBX::RbxInstance localPlayer(0);
 }
